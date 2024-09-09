9e2e57e2833bc4f1bb31b68c5d6fa57d033a13c2
chorst <chorst@student.42heilbronn.de>
2024-09-09 10:10:43 +0200

    morning commit to pull and merge stuff

--- src/error_handler/error_checks.c
+++ src/error_handler/error_checks.c
@@ -6,7 +6,7 @@
 /*   By: chorst <chorst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 10:55:01 by chorst            #+#    #+#             */
-/*   Updated: 2024/09/05 15:03:51 by chorst           ###   ########.fr       */
+/*   Updated: 2024/09/09 10:09:36 by chorst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	error_checks(t_data *data)
 {
 	if (check_nonsense(*data))
 		return (error_msg("There is nonsense in the file!"));
-	if (check_directions(*data))
+	else if (check_directions(*data))
 		return (error_msg("Missing or doubled directions"));
-	if (check_rgb(*data) == 1)
+	else if (check_rgb(*data) == 1)
 		return (error_msg("There is nonsense in RGB Values!"));
-	if (check_rgb(*data) == 2)
+	else if (check_rgb(*data) == 2)
 		return (error_msg("RGB values are out of range!"));
 	// if (check_order(*data))
 	// 	return (error_msg("Wrong order of elements. Map need to be last"));
